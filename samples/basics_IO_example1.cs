ad79000986174f72241a9040825469e425741c35
jindraivanek <jindra.ivanek@gmail.com>
2017-05-25 08:30:11 +0200

    Add sample.

---
+++ samples/basics_IO_example1.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read every line in the file:" + Environment.NewLine); 
+            using (var reader = new StreamReader("basics_IO_example1.cs"))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);   
+                }
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            Console.ReadLine();
+            
+            Console.WriteLine("Read all line in the file:" + Environment.NewLine);
+            string file = File.ReadAllText(@"basics_IO_example1.cs");
+            Console.WriteLine(file);
+
+            Console.WriteLine(Environment.NewLine);
+            Console.ReadLine();
+
+            Console.WriteLine("Read all line in the file:" + Environment.NewLine);
+
+            string[] lines = File.ReadAllLines(@"basics_IO_example1.cs");
+            Console.WriteLine("Line Count: "+ lines.Length);
+            foreach (string line in lines)
+            {   
+                if (line.Length > 100)
+                {
+                    Console.WriteLine("line length is greater than 100");
+                }
+
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
