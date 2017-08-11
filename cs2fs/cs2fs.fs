db715438b5b9980dbd6d35c09e6d476e2bd68916
jindraivanek <jindra.ivanek@gmail.com>
2017-08-11 19:59:03 +0200

    Move extra ActivePatterns to  separate file

--- cs2fs/cs2fs.fs
+++ cs2fs/cs2fs.fs
@@ -3,69 +3,9 @@ module cs2fs.Main
 open Microsoft.CodeAnalysis
 open Microsoft.CodeAnalysis.CSharp
 open cs2fs.AST
-
+open cs2fs.CSharpActivePatternsExtra
 let sequence xs = xs |> Seq.toList |> ExprSequence
 let (|++|) x y = ExprSequence [x;y]
-
-let (|VariableDeclarationSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.VariableDeclarationSyntax as node ->
-      Some (node.Type, node.Variables |> Seq.toList)
-    | _ -> None
-    
-let (|VariableDeclaratorSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.VariableDeclaratorSyntax as node ->
-      Some (node.Identifier, node.ArgumentList, Option.ofObj node.Initializer)
-    | _ -> None
-    
-let (|ParameterListSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.ParameterListSyntax as node ->
-      Some (node.OpenParenToken, node.Parameters |> Seq.toList, node.CloseParenToken)
-    | _ -> None
-
-let (|TypeParameterListSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.TypeParameterListSyntax as node ->
-      Some (node.LessThanToken, node.Parameters |> Seq.toList, node.GreaterThanToken)
-    | _ -> None
-
-let (|ArgumentListSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.ArgumentListSyntax as node ->
-      Some (node.OpenParenToken, node.Arguments |> Seq.toList, node.CloseParenToken)
-    | _ -> None
-
-let (|BracketedArgumentListSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.BracketedArgumentListSyntax as node ->
-      Some (node.Arguments |> Seq.toList)
-    | _ -> None
-
-let (|FieldDeclarationSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.FieldDeclarationSyntax as node ->
-      Some (node.AttributeLists |> Seq.toList, node.Declaration, node.Modifiers |> Seq.toList)
-    | _ -> None
-
-let (|InitializerExpressionSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.InitializerExpressionSyntax as node ->
-      Some (node.Expressions |> Seq.toList)
-    | _ -> None
-
-let (|ArrayCreationExpressionSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.ArrayCreationExpressionSyntax as node ->
-      Some (node.Type.ElementType, node.Type.RankSpecifiers |> Seq.toList)
-    | _ -> None
-
-let (|ForStatementSyntax|_|) (node:Microsoft.CodeAnalysis.SyntaxNode) =
-    match node with
-    | :? Microsoft.CodeAnalysis.CSharp.Syntax.ForStatementSyntax as node ->
-      Some (node.Declaration, node.Initializers |> Seq.toList, node.Condition, node.Incrementors |> Seq.toList, node.Statement)
-    | _ -> None
     
 let rec getParentOfType<'t when 't :> SyntaxNode> (node: SyntaxNode) =
     match node.Parent with
